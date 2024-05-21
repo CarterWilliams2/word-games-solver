41dbd19d9b6da9532961fa22238d3fc4f0be211e
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2024-05-21 13:33:31 -0500

    Make initiliaze board function

--- WordHuntSolver.java
+++ WordHuntSolver.java
@@ -1,2 +1,21 @@
 public class WordHuntSolver{
+
+//initialize variables
+   private String[][] gameBoard;
+   private int width;
+   private int height;
+
+   public WordHuntSolver() {
+      initializeBoard(gameBoard);
+      width = gameBoard.length;
+      height = width;
+   }
+   
+   public void initializeBoard(String[][] gameBoard) {
+      for (int i=0; i<4; i++) {
+         for (int j=0; j<4; j++) {
+            gameBoard[i][j] = null;
+         }
+      }
+   }
 }
