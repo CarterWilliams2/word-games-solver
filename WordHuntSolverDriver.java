327d1c87dae68d4ba115256c4814b996281d79be
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2024-05-21 14:13:01 -0500

    Test functionality of set board function

---
+++ WordHuntSolverDriver.java
@@ -0,0 +1,8 @@
+public class WordHuntSolverDriver {
+
+   public static void main(String[] args) {
+      WordHuntSolver game = new WordHuntSolver();
+      String[] board = new String[] {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"};
+      game.setBoard(board);
+   }
+}
