947225007f9a685aacb07ffe897aa35b2b4a1e2f
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2024-05-21 13:44:51 -0500

    Make set board function

--- WordHuntSolver.java
+++ WordHuntSolver.java
@@ -1,3 +1,6 @@
+import java.io.FileReader;
+import java.io.File;
+
 public class WordHuntSolver{
 
 //initialize variables
@@ -6,15 +9,30 @@ public class WordHuntSolver{
    private int height;
 
    public WordHuntSolver() {
-      initializeBoard(gameBoard);
+      initializeBoard();
       width = gameBoard.length;
       height = width;
    }
    
-   public void initializeBoard(String[][] gameBoard) {
+   //this sets the game board to be null in all indexes
+   public void initializeBoard() {
       for (int i=0; i<4; i++) {
          for (int j=0; j<4; j++) {
             gameBoard[i][j] = null;
+         }
+      }
+   }
+   
+   //this allows the game board to be set by the user
+   public void setBoard(String[] letters) {
+      if (letters.length != 16) {
+         return;
+      }
+      for (int i=0; i<16; i++) {
+         for (int j=0; j<4; j++) {
+            for (int k=0; k<4; k++) {
+               gameBoard[j][k] = letters[i];
+            }
          }
       }
    }
